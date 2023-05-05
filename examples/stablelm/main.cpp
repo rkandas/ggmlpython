c2924ae5b813b31b5fc3953339356b1f78600b98
rkandas <53561723+rkandas@users.noreply.github.com>
2023-05-05 19:49:16 +0530

    [Raju] - trim

--- examples/stablelm/main.cpp
+++ examples/stablelm/main.cpp
@@ -674,13 +674,10 @@ int main(int argc, char ** argv) {
 }
 
 std::string run_inference(gpt_params params, bool loadmodel) {
-    const int64_t t_main_start_us = ggml_time_us();
     result = "";
     if (params.seed < 0) {
             params.seed = time(NULL);
     }
-
-    //printf("%s: seed = %d\n", __func__, params.seed);
 
     std::mt19937 rng(params.seed);
 
@@ -689,20 +686,11 @@ std::string run_inference(gpt_params params, bool loadmodel) {
     
     int n_past = 0;
 
-    int64_t t_sample_us  = 0;
-    int64_t t_predict_us = 0;
-
     std::vector<float> logits;
-    //fprintf(stderr, "%s: prompt received '%s'\n", __func__, params.prompt.c_str());
     // tokenize the prompt
     std::vector<gpt_vocab::id> embd_inp = ::gpt_tokenize(vocab, params.prompt.c_str());
 
     params.n_predict = std::min(params.n_predict, model.hparams.n_ctx - (int) embd_inp.size());
-
-    /* printf("%s: number of tokens in prompt = %zu\n", __func__, embd_inp.size());
-    for (int i = 0; i < embd_inp.size(); i++) {
-        printf("%s: token[%d] = %6d, %s\n", __func__, i, embd_inp[i], vocab.id_to_token.at(embd_inp[i]).c_str());
-    } */
 
     std::vector<gpt_vocab::id> embd;
 
@@ -720,14 +708,9 @@ std::string run_inference(gpt_params params, bool loadmodel) {
     for (int i = embd.size(); i < embd_inp.size() + params.n_predict; i++) {
         // predict
         if (embd.size() > 0) {
-            const int64_t t_start_us = ggml_time_us();
             if (!stablelm_eval(model, params.n_threads, n_past, embd, logits, mem_per_token)) {
-                //printf("Failed to predict\n");
                 return "";
             }
-
-
-            t_predict_us += ggml_time_us() - t_start_us;
         }
 
         n_past += embd.size();
@@ -738,19 +721,9 @@ std::string run_inference(gpt_params params, bool loadmodel) {
             const int   top_k = params.top_k;
             const float top_p = params.top_p;
             const float temp  = params.temp;
-
             const int n_vocab = model.hparams.n_vocab;
-            //printf("%s: n_vocab = %d", __func__, n_vocab);
             gpt_vocab::id id = 0;
-
-            {
-                const int64_t t_start_sample_us = ggml_time_us();
-
-                id = gpt_sample_top_k_top_p(vocab, logits.data() + (logits.size() - n_vocab), top_k, top_p, temp, rng);
-                //printf("%s: %d", __func__,id);
-                t_sample_us += ggml_time_us() - t_start_sample_us;
-            }
-
+            id = gpt_sample_top_k_top_p(vocab, logits.data() + (logits.size() - n_vocab), top_k, top_p, temp, rng);
             // add it to the context
             embd.push_back(id);
         } else {
@@ -766,7 +739,6 @@ std::string run_inference(gpt_params params, bool loadmodel) {
 
         // display text
         for (auto id : embd) {
-            //printf("%s", vocab.id_to_token[id].c_str());
             result += vocab.id_to_token[id];
         }
         fflush(stdout);
@@ -775,17 +747,6 @@ std::string run_inference(gpt_params params, bool loadmodel) {
         if (embd.back() == 0) {
             break;
         }
-    }
-
-    // report timing
-    {
-        const int64_t t_main_end_us = ggml_time_us();
-
-        /* printf("\n\n");
-        printf("%s: mem per token = %8zu bytes\n", __func__, mem_per_token);
-        printf("%s:   sample time = %8.2f ms\n", __func__, t_sample_us/1000.0f);
-        printf("%s:  predict time = %8.2f ms / %.2f ms per token\n", __func__, t_predict_us/1000.0f, t_predict_us/1000.0f/n_past);
-        printf("%s:    total time = %8.2f ms\n", __func__, (t_main_end_us - t_main_start_us)/1000.0f); */
     }
 
     if(loadmodel)
